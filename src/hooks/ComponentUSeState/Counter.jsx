5b9c6e61913104ef7fa525273ac20e4c4842aff8
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-19 01:31:58 +0530

    add and minus

--- src/hooks/ComponentUSeState/Counter.jsx
+++ src/hooks/ComponentUSeState/Counter.jsx
@@ -4,26 +4,44 @@ import './style.css';
 const Counter = () => {
 
     const [count, setCount] = useState(0);
+    const [inputValue, setInputValue] = useState(0);
+
 
     const increment = () =>{
-        return (
-            setCount(count + 1)
-        )
+            
+            if(inputValue !== 0 && inputValue !== "" && count < 100){
+                setCount(parseInt(count) + parseInt(inputValue))
+                return
+            }
+            
+            if (count < 100) {
+                setCount(parseInt(count) + 1);
+            }
+        
     }
 
     const decrement = () =>{
-        return (
-            setCount(count - 1)
-        )
+        if(inputValue !== 0 && inputValue !== "" && count > 0){
+                setCount(parseInt(count) - parseInt(inputValue))
+                return
+            }
+            
+            if (count > 0) {
+                setCount(parseInt(count) - 1);
+            }
     }
     return(
         <>
-            <div class="card">
-                <h2 class="card-title">useState Counter</h2>
-                <p class="counter-display">Count: <span id="count">{count}</span></p>
-                <div class="button-group">
-                    <button id="decrement" class="counter-button" onClick={decrement}>- Counter</button>
-                    <button id="increment" class="counter-button" onClick={increment}>+ Counter</button>
+            <div className="card">
+                <h2 className="card-title">useState Counter</h2>
+                <p className="counter-display">Count: <span id="count">{count}</span></p>
+                <div className="input-container">
+                    <input type="text" id="styled-input" min="0" value={inputValue} onChange={ (e) => setInputValue(e.target.value)}/>
+                    <label htmlFor="styled-input" className="input-label">Add Value</label>
+                </div>
+                <div className="button-group">
+                    <button id="decrement" className="counter-button" onClick={decrement}>- Counter</button>
+                    <button id="increment" className="counter-button" onClick={increment}>+ Counter</button>
                 </div>
             </div>
         </>
