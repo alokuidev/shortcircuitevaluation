38aa1cfd2d1a99142c1b871cf7f73f87a923f987
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-23 03:17:30 +0530

    login page added

--- src/hooks/ComponentUseRef/Useref.jsx
+++ src/hooks/ComponentUseRef/Useref.jsx
@@ -1,6 +1,27 @@
+import './Useref.css'
 const Useref = () =>{
     return(
-        <h1>This is UseRef Page</h1>
+        <>
+    <div className="login-container">
+    <div className="login-box">
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required/>
+        </div>
+        <button type="submit" className="login-button">Login</button>
+        <div className="form-footer">
+          <p>Don't have an account? <a href="javasctipt:void(0)">Sign Up</a></p>
+        </div>
+      </form>
+    </div>
+  </div>
+        </>
     )
 }
 
