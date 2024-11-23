607cfe396b3b06f538aa69899fbc23deb2b7623e
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-23 23:55:06 +0530

    appending child twice to check the value

--- src/hooks/ComponentUseRef/ForwardrefChild.jsx
+++ src/hooks/ComponentUseRef/ForwardrefChild.jsx
@@ -1,13 +1,12 @@
 import { forwardRef, useId } from "react"
 
-export const ChildRef = forwardRef((prop, ref) =>{
+export const ChildRef = forwardRef((props, ref) =>{
     const id = useId();
     return(
         <>
         
-            <label htmlFor={id}></label>
+            <label htmlFor={id}>{props.label}</label>
             <input type="text" ref={ref}/>
-            <button type="submit" className="login-button">Login</button>
        
         </>
     )
--- src/hooks/ComponentUseRef/ForwardrefParent.jsx
+++ src/hooks/ComponentUseRef/ForwardrefParent.jsx
@@ -4,15 +4,17 @@ import { ChildRef } from "./ForwardrefChild";
 const RefFun = () => {
 
     const userid = useRef(null);
-
+    const pass = useRef(null);
     const formSubmit = (e) =>{
         e.preventDefault();
-        console.log(userid.current.value);   
+        console.log(userid.current.value, pass.current.value);   
     } 
     return(
         <>
         <form onSubmit={formSubmit} >
-            <ChildRef label="username" ref={userid} />
+            <ChildRef label="Username" ref={userid} />
+            <ChildRef label="Password" ref={pass} /> 
+            <button type="submit" className="login-button">Login</button>
         </form>    
         </>
     )
