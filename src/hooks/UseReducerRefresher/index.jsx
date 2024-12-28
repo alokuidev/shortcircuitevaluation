de937c151f2a2d9a82d77a00f19f412bccadc276
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-28 20:50:45 +0530

    optimized code

--- src/hooks/UseReducerRefresher/index.jsx
+++ src/hooks/UseReducerRefresher/index.jsx
@@ -1,33 +1,54 @@
 import { useReducer } from 'react'
 import './style.css'
 export default function UseReducerRefresher(){
-    const merafun = (state,action) =>{
-        //const { action } = action;
-        console.log(action)
-        switch (action) {
-            case 'decrement':
-                return state - 1;
-            case 'increment':
-                return state + 1;
-            default: 
-                console.log('No action');   
-                break
-        }
+
+    const initialState ={
+        count:0,
+        action:'Yet to trigger a action'
     }
-    const [count, dispatch] = useReducer(merafun, 0);
-    //console.log(useReducer());
+
+
+    const myFun = (state, action) =>{
+        const {type} = action
+
+        switch (type) {
+            case 'increment':
+                return {
+                     ...state,
+                      count: state.count + 1,
+                      action: 'Incrementing'  
+                    };
+            case 'decrement':
+                return {
+                    ...state,
+                    count: state.count - 1,
+                    action: 'decrementing'
+                };
+        
+            default:
+                return {
+                    ...state,
+                    count:0,
+                    action:'Reset'
+                };
+        }
+        
+    }
+    const [ state, dispatch ] = useReducer(myFun, initialState)
     return(
         <>
             <div className="card">
                 <h2 className="card-title">useState Counter</h2>
-                <p className="counter-display">Count: <span id="count">{count}</span></p>
+                <p className="counter-display" style={{fontSize:'14px'}}>Action: <span id="count">{state.action}</span></p>
+                <p className="counter-display">Count: <span id="count">{state.count}</span></p>
                 <div className="input-container">
                     <input type="text" id="styled-input" min="0" value="" />
                     <label htmlFor="styled-input" className="input-label">Add Value</label>
                 </div>
                 <div className="button-group">
-                    <button id="decrement" className="counter-button" disabled={count === 0} onClick={() => dispatch('decrement')} >- Counter</button>
-                    <button id="increment" className="counter-button" onClick={() => dispatch('increment')} >+ Counter</button>
+                    <button id="decrement" className="counter-button" disabled={ state.count === 0} onClick={() => dispatch({type:'decrement'})}>- Counter</button>
+                    <button id="increment" className="counter-button" style={{backgroundColor:'blue'}} onClick={() => dispatch({type:'reset'})} >Reset</button>
+                    <button id="increment" className="counter-button" onClick={() => dispatch({type:'increment'})} >+ Counter</button>
                 </div>
             </div>
         </>
