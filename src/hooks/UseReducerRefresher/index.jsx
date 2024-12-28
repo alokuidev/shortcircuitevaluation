8b73016c617753a04d57bcd8517b605deca6ab10
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-29 00:17:29 +0530

    rewriting code

--- src/hooks/UseReducerRefresher/index.jsx
+++ src/hooks/UseReducerRefresher/index.jsx
@@ -1,54 +1,49 @@
-import { useReducer } from 'react'
-import './style.css'
+import { useReducer } from 'react';
+import './style.css';
 export default function UseReducerRefresher(){
 
     const initialState ={
         count:0,
-        action:'Yet to trigger a action'
+        action:'Yet To Trigger A Action'
     }
 
+    const reducer = (state, action) =>{
+            const { type } = action;
 
-    const myFun = (state, action) =>{
-        const {type} = action
-
-        switch (type) {
-            case 'increment':
-                return {
-                     ...state,
-                      count: state.count + 1,
-                      action: 'Incrementing'  
-                    };
-            case 'decrement':
-                return {
-                    ...state,
-                    count: state.count - 1,
-                    action: 'decrementing'
-                };
-        
-            default:
-                return {
-                    ...state,
-                    count:0,
-                    action:'Reset'
-                };
-        }
-        
+            switch (type) {
+                case 'decrement':
+                    return{
+                        ...state,
+                        count: state.count - 1,
+                        action:'Decrementing'
+                    }
+                    case 'increment':
+                        return{
+                            ...state,
+                            count: state.count + 1,
+                            action:'Incrementing'
+                    }
+                    case 'reset':
+                        return{
+                            ...state,
+                            count: 0,
+                            action:'Reset'
+                    }     
+                default:
+                    break;
+            }
     }
-    const [ state, dispatch ] = useReducer(myFun, initialState)
-    return(
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
         <>
             <div className="card">
                 <h2 className="card-title">useState Counter</h2>
-                <p className="counter-display" style={{fontSize:'14px'}}>Action: <span id="count">{state.action}</span></p>
+                <p className="counter-display" style={{fontSize:'12px', color:'red'}}>Action: <span id="count">{state.action}</span></p>
                 <p className="counter-display">Count: <span id="count">{state.count}</span></p>
-                <div className="input-container">
-                    <input type="text" id="styled-input" min="0" value="" />
-                    <label htmlFor="styled-input" className="input-label">Add Value</label>
-                </div>
                 <div className="button-group">
-                    <button id="decrement" className="counter-button" disabled={ state.count === 0} onClick={() => dispatch({type:'decrement'})}>- Counter</button>
-                    <button id="increment" className="counter-button" style={{backgroundColor:'blue'}} onClick={() => dispatch({type:'reset'})} >Reset</button>
-                    <button id="increment"  className="counter-button" onClick={() => dispatch({type:'increment'})} >+ Counter</button>
+                    <button id="decrement" className="counter-button" disabled={state.count ===0} onClick={() => dispatch({type:'decrement'})}>- Counter</button>
+                    <button id="increment" className="counter-button" onClick={() => dispatch({type:'increment'})} >+ Counter</button>
+                    <button id="increment" style={{backgroundColor:'blue'}} className="counter-button" onClick={() => dispatch({type:'reset'})} >Reset</button>
                 </div>
             </div>
         </>
